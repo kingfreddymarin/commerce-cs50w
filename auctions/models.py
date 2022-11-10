85196263d59c9d29efdbfe3e4c3f388fc4605209
Freddy Marin <freddymarin.jpg@gmail.com>
2022-11-10 13:01:33 -0600

    created Listing and category model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,25 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.categoryName
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=50, default="")
+    description = models.CharField(max_length=280, default="")
+    image = models.CharField(max_length=2090, default="")
+    price = models.FloatField(default=0)
+    activeListing = models.BooleanField(default=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+
+    def __str__(self):
+        return self.title
